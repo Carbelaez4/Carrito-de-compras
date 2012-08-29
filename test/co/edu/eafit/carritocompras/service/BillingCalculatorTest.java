590aa8f98492f1df89f2c84b4400ce39ef9b7bc8
unknown <carbelaez4@gmail.com>
2012-08-29 18:23:55 -0500

    Solucion punto tres IVA

--- test/co/edu/eafit/carritocompras/service/BillingCalculatorTest.java
+++ test/co/edu/eafit/carritocompras/service/BillingCalculatorTest.java
@@ -12,26 +12,36 @@ import org.mockito.Mockito;
 import co.edu.eafit.carritocompras.data.Customer;
 import co.edu.eafit.carritocompras.data.Purchase;
 import co.edu.eafit.carritocompras.data.Product;
-import co.edu.eafit.carritocompras.data.products.ElectronicProduct;
-import co.edu.eafit.carritocompras.data.products.Furniture;
+import co.edu.eafit.carritocompras.service.TaxCalculator;
 
 public class BillingCalculatorTest {
 	
 	private Customer customer;
+	private Purchase p;
 	
 	@Before
 	public void setUp() {
 		Customer customer = new Customer("001", "camilo");
+		p = BillingCalculator.calculateTotalPurchase(customer, "EL01,FU01");
 	}
 	
 	@Test
 	public void testCalculateTotalPurchase() {
-		Purchase p = BillingCalculator.calculateTotalPurchase(customer, "EL01,FU01");
 		List<Product> products = p.getProducts();
 		BigDecimal aPrice = products.get(0).getPrice().subtract(products.get(0).calculateDiscount());
 		BigDecimal bPrice = products.get(1).getPrice().subtract(products.get(1).calculateDiscount());
 		
 		Assert.assertEquals(aPrice.add(bPrice), p.getTotalPrice());
+	}
+	
+	@Test
+	public void testCalculateIva() {
+		TaxCalculator taxCalculator = Mockito.mock(TaxCalculator.class);
+		BigDecimal iva = new BigDecimal(1.15);
+		
+		Mockito.when(taxCalculator.calculateIva(p)).thenReturn(iva.multiply(p.getTotalPrice()));
+		
+		Assert.assertEquals(iva.multiply(p.getTotalPrice()), taxCalculator.calculateIva(p));
 	}
 	
 }
